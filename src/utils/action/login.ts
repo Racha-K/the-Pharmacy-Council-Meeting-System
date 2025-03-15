7e31f7ab00bfa297a9a424f8252bd7bd25e5e76c
Racha-K <rachapon.telehealth@gmail.com>
2025-03-15 22:11:37 +0700

    fix

--- src/utils/action/login.ts
+++ src/utils/action/login.ts
@@ -25,14 +25,14 @@ export async function login({ license, password }: LoginProps) {
   const data = await response.json();
   if (data.error_code === 0) {
     (await cookies()).set("access_token", data.data.access_token, {
-      httpOnly: true,
-      secure: true,
+      // httpOnly: true,
+      // secure: true,
       maxAge: 60 * 60 * 24,
       path: "/",
     });
     (await cookies()).set("license_id", data.data.license_id, {
-      httpOnly: true,
-      secure: true,
+      // httpOnly: true,
+      // secure: true,
       maxAge: 60 * 60 * 24,
       path: "/",
     });
